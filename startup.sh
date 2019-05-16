48a5aba2dacb2b904b46122de2f46ed1dc96be99
熊迪 <d@huiqiait.com>
2019-05-17 02:33:44 +0800

    add

--- startup.sh
+++ startup.sh
@@ -42,3 +42,30 @@ ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpZZjVHd1FzQ2FEU0w=@34.97.149.185:42708/?outli
 
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTo3cllxMkluaGo4Z0s=@68.183.162.247:1873/?outline=1
 
+长城
+
+1. 香港 75ms
+2. 新加坡 98ms
+3. 大版 98ms
+
+电信
+
+1. 香港 69ms
+2. 新加坡 114ms
+3. 大版 128ms
+
+联通
+
+1. 香港 70ms
+2. 新加坡 118ms
+3. 大版 136ms
+
+GCP SS HongKong
+
+{"apiUrl":"https://34.92.16.79:3584/kM6o1VxHLAgFHKr5QdjfGg","certSha256":"EC64914C99E356C34D34C67CD02512B8E7ED8626B
+73C6332DE358F8027832AE3"}
+
+https://s3.amazonaws.com/outline-vpn/invite.html?admin_embed#/zh-CN/invite/ss%3A%2F%2FY2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpMclo3OTgzZHFabkw%3D%4034.92.16.79%3A63440%2F%3Foutline%3D1
+
+
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpMclo3OTgzZHFabkw=@34.92.16.79:63440/?outline=1
